e6e324245756ab6283b2ab6ba907893a97b7087f
amgits71 <mikryukova.yana71@gmail.com>
2023-05-14 21:54:59 +0500

    On branch master Your branch is up to date with 'origin/master'.
    hanges to be committed:
    modified:   hello/models.py

--- hello/models.py
+++ hello/models.py
@@ -19,21 +19,3 @@ class Users(models.Model):
     study = models.CharField(max_length = 50, choices = answer)
     work = models.CharField(max_length = 50, choices = answer)
     
-class Category(models.Model): 
-    name = models.CharField(max_length=100)
-    class Meta:
-    	verbose_name = ("Category")
-        verbose_name_plural = ("Categories")
-    def __str__(self):
-        return self.name  
-    
-class TodoList(models.Model):
-	title = models.CharField(max_length=250)
-	content = models.TextField(blank=True)
-	created = models.DateField(default=timezone.now().strftime("%Y-%m-%d")) #дата создания
-	due_date = models.DateField(default=timezone.now().strftime("%Y-%m-%d")) #до какой даты нужно было сделать дело
-	category = models.ForeignKey(Category, default="general",on_delete=models.PROTECT) # foreignkey с помощью которой мы будем осуществлять связь с таблицей Категорий
-	class Meta: #используем вспомогательный класс мета для сортировки наших дел
-        ordering = ["-created"] #сортировка дел по времени их создания
-	def __str__(self):
-    	return self.title
